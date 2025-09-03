451c706e0824bba82355b16e986988cbbef83593
Tomas Pecukevicius <pecukevicius@gmail.com>
2025-09-03 20:40:48 +0100

    Updated npm packages

--- build/_app/immutable/chunks/Bi-dsBaK.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_13qmkgr?.base??"/pecuk.dev",e=globalThis.__sveltekit_13qmkgr?.assets??s;export{e as a,s as b};
---
+++ build/_app/immutable/chunks/CE9qaogm.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_h11dyt?.base??"/pecuk.dev",e=globalThis.__sveltekit_h11dyt?.assets??s;export{e as a,s as b};
